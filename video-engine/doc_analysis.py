e397e773757574d3a995ef1c979f4ddd662e99bd
kyliekndUeda <kylieknd07@gmail.com>
2026-06-01 14:07:15 -0400

    Minor changes on text extraction feature

--- video-engine/doc_analysis.py
+++ video-engine/doc_analysis.py
@@ -18,23 +18,30 @@ from pathlib import Path
 import os
 
 # Get the type of input
-inputFile = "topo_lec.pdf" # for test
+inputFile = "topo_lec.pdf" # for test (single-page pdf)
+inputFile = "cs322_bfs.pdf" # for test (multiple-page pdf)
 fType = Path(inputFile).suffix # returns ".pdf", ".jpg", etc.
+
+# Create a new folder to store the output
+os.makedirs("output_sample", exist_ok=True)
 
 ############### Format Checker ###############
 # if pdf, return pdf
 if fType == '.pdf':
     pdf = inputFile
+    # Create a subfolder for each input
+    output_name = os.path.splitext(os.path.basename(inputFile))[0]  # gets "topo_lec" from "topo_lec.pdf"
+    os.makedirs(f"output_sample/{output_name}", exist_ok=True)
 # else, show error msg (Image format needs OCR)
 else:
     print("Error: Unable to proccess this format.")
     exit()
 
 ############### Text Extraction (markdown-styled) ###############
-md_text = pymupdf4llm.to_markdown(pdf, write_images=True, image_path="output_sample/") 
+md_text = pymupdf4llm.to_markdown(pdf, write_images=True, image_path=f"output_sample/{output_name}") 
 # print("Here's what's converted into markdown!\n\n", md_text) # for testing
 
 ############### Write extracted text in.md File ###############
 output_name = os.path.splitext(os.path.basename(inputFile))[0]  # gets "topo_lec" from "topo_lec.pdf"
-with open(f"output_sample/{output_name}.md", "w") as f:
+with open(f"output_sample/{output_name}/{output_name}.md", "w") as f:
     f.write(md_text)
